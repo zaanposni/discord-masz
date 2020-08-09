db63773cb3355eac2b76607a6ef50c87387a282f
Samuel <kraesamu@icloud.com>
2020-08-09 03:53:36 +0200

    Added template engine twig; Added basic html structure and main routes;

--- webinterface/config/bundles.php
+++ webinterface/config/bundles.php
@@ -3,4 +3,7 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
--- webinterface/webpack.config.js
+++ webinterface/webpack.config.js
@@ -64,7 +64,7 @@ Encore
     //.enableIntegrityHashes(Encore.isProduction())
 
     // uncomment if you're having problems with a jQuery plugin
-    //.autoProvidejQuery()
+    // .autoProvidejQuery()
 
     // uncomment if you use API Platform Admin (composer req api-admin)
     //.enableReactPreset()
