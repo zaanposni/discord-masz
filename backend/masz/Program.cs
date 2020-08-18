29e0774b0e79f51c95e52615f3a69331750d5500
zaanposni <zaanposni@users.noreply.github.com>
2020-08-18 18:32:11 +0200

    added nginx reverse proxy to docker compose

--- backend/masz/Program.cs
+++ backend/masz/Program.cs
@@ -21,7 +21,7 @@ namespace masz
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                    webBuilder.UseUrls("http://0.0.0.0:5565/");
+                    webBuilder.UseUrls("http://0.0.0.0:80/");
                 });
     }
 }
