dfa7b5a54351f1b8d8cb2b2c4c4e94a506df85be
zaanposni <zaanposni@users.noreply.github.com>
2020-09-18 15:56:50 +0200

    added test

--- tests/test_smoketest.py
+++ tests/test_smoketest.py
@@ -10,3 +10,12 @@ def test_ping_frontend():
 def test_ping_backend():
     r = requests.get("http://127.0.0.1:5565/api/v1/status")
     assert r.status_code == 200
+
+def test_healthcheck_backend():
+    headers = {
+        "Accept": "application/json"
+    }
+    r = requests.get("http://127.0.0.1:5565/api/v1/status", headers=headers)
+    assert r.status_code == 200
+    json_value = r.json()
+    assert str(json_value["status"]).lower() == "ok"
