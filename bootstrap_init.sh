cb3c42899e44c30ea6926ca79c53ad6fd442856a
zaanposni <zaanposni@users.noreply.github.com>
2020-09-18 17:26:51 +0200

    fix

--- bootstrap_init.sh
+++ bootstrap_init.sh
@@ -1,12 +1,14 @@
 echo "Copying config in subdirectories for docker"
 if [ ! -f ./config.json ]; then
     cp default-config.json ./backend/
+    file=default-config.json
 else
     cp config.json ./backend/
+    file=config.json
 fi
 
 echo "Using specified nginx config"
-if [[ $(jq '.meta.nginx_mode' config.json) = *prod* ]]; then
+if [[ $(jq '.meta.nginx_mode' $file.json) = *prod* ]]; then
     echo "prod"
     cp ./nginx/nginx-prod.conf ./nginx/nginx.conf
 else
