f85ffa700351c34d97c66ac92e777939b2340f90
zaanposni <zaanposni@users.noreply.github.com>
2020-09-25 18:24:01 +0200

    fixes #66

--- bootstrap_init.sh
+++ bootstrap_init.sh
@@ -7,19 +7,19 @@ else
     file=config.json
 fi
 
-mysql_port=$(jq '.mysql_database.port')
-mysql_database=$(jq '.mysql_database.database')
-mysql_user=$(jq '.mysql_database.user')
-mysql_pass=$(jq '.mysql_database.pass')
-mysql_root_pass=$(jq '.mysql_database.root_pass')
+mysql_port=$(jq  --raw-output '.mysql_database.port' $file)
+mysql_database=$(jq --raw-output '.mysql_database.database' $file)
+mysql_user=$(jq --raw-output '.mysql_database.user' $file)
+mysql_pass=$(jq --raw-output '.mysql_database.pass' $file)
+mysql_root_pass=$(jq --raw-output '.mysql_database.root_pass' $file)
 
-echo "\n " \
-"MYSQL_PORT=$mysql_port" \
-"MYSQL_DATABASE=$mysql_database" \
-"MYSQL_USER=$mysql_user" \
-"MYSQL_PASSWORD=$mysql_pass" \
-"MYSQL_ROOT_PASSWORD=$mysql_root_pass" \
-"" > .env
+cat> .env <<- EOM
+MYSQL_PORT=$mysql_port
+MYSQL_DATABASE=$mysql_database
+MYSQL_USER=$mysql_user
+MYSQL_PASSWORD=$mysql_pass
+MYSQL_ROOT_PASSWORD=$mysql_root_pass
+EOM
 
 cat .env
 
