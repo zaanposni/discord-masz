cdd2b6dc04bc62ef34ccc67581e0445bbdb2e69d
zaanposni <zaanposni@users.noreply.github.com>
2020-09-25 18:37:24 +0200

    .

--- bootstrap_init.sh
+++ bootstrap_init.sh
@@ -7,6 +7,8 @@ else
     file=config.json
 fi
 
+jq --help > /dev/null || echo "jq is not installed, please install it."; exit 1
+
 mysql_port=$(jq  --raw-output '.mysql_database.port' $file)
 mysql_database=$(jq --raw-output '.mysql_database.database' $file)
 mysql_user=$(jq --raw-output '.mysql_database.user' $file)
