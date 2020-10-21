012b5418fdef88f417274ec1fcec19e9f831fb15
zaanposni <zaanposni@googlemail.com>
2020-10-21 20:32:57 +0200

    fixed footer and button alignment

--- webinterface/app/src/Config/Config.php
+++ webinterface/app/src/Config/Config.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpClient\HttpClient;
 class Config
 {
     public static function GetBaseUrl() {
-        //return 'http://127.0.0.1:5565';  // for local development
+        // return 'http://127.0.0.1:5565';  // for local development
         return 'http://masz_backend:80';  // for docker deployment
     }
 }
