a08dcc21043e340cbb126d5ab1fd3e9495671c84
zaanposni <me@zaanposni.com>
2020-11-19 18:54:51 +0100

    fix deletion of modcase with comments, fixes #80

--- backend/masz/Dtos/ModGuild/GuildConfigForCreateDto.cs
+++ backend/masz/Dtos/ModGuild/GuildConfigForCreateDto.cs
@@ -12,8 +12,10 @@ namespace masz.Dtos.GuildConfig
         public string AdminRoleId { get; set; }
         public bool ModNotificationDM { get; set; }
         [Url(ErrorMessage = "Webhook needs to be a valid url")]
+        [RegularExpression(@"^https://discordapp.com/.*$", ErrorMessage = "please specify a url that starts with 'https://discordapp.com/'.")]
         public string ModPublicNotificationWebhook { get; set; }
         [Url(ErrorMessage = "Webhook needs to be a valid url")]
+        [RegularExpression(@"^https://discordapp.com/.*$", ErrorMessage = "please specify a url that starts with 'https://discordapp.com/'.")]
         public string ModInternalNotificationWebhook { get; set; }
     }
 }
