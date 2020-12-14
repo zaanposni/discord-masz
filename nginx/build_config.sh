eb15649027d90e7561d8718503922ba585767c59
zaanposni <me@zaanposni.com>
2020-12-14 11:29:06 +0100

    fix

--- nginx/build_config.sh
+++ nginx/build_config.sh
@@ -4,12 +4,7 @@ if [[ -z "$DEPLOY_DOMAIN" ]]; then
     echo "deploying local nginx"
     cp nginx-local.conf nginx.conf
 else
-if [[ $DEPLOY_MODE == "prod" ]]; then
-        echo "deploying prod nginx with domain $DEPLOY_DOMAIN"
-        sed -i -e 's/{{placeholder}}/'$DEPLOY_DOMAIN'/g' ./nginx-prod.conf
-        cp nginx-prod.conf nginx.conf
-    else 
-        echo "deploying local nginx"
-        cp nginx-local.conf nginx.conf
-    fi
+    echo "deploying prod nginx with domain $DEPLOY_DOMAIN"
+    sed -i -e 's/{{placeholder}}/'$DEPLOY_DOMAIN'/g' ./nginx-prod.conf
+    cp nginx-prod.conf nginx.conf
 fi
