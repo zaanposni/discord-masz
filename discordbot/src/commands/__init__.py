d68bcd7f4eba9e203223312cac2e2630de9832e7
zaanposni <me@zaanposni.com>
2020-12-17 22:40:33 +0100

    added register command

--- discordbot/src/commands/__init__.py
+++ discordbot/src/commands/__init__.py
@@ -1,6 +1,7 @@
 from .version import version
 from .url import url
 from .features import features
+from .register import register
 
-ALL_COMMANDS = [ version, url, features ]
+ALL_COMMANDS = [ version, url, features, register ]
 
