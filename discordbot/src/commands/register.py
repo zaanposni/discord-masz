7f97b9cb421b9070e771d834615b5049abb4f9b7
zaanposni <me@zaanposni.com>
2020-12-24 22:24:15 +0100

    fixes #200

--- discordbot/src/commands/register.py
+++ discordbot/src/commands/register.py
@@ -6,6 +6,6 @@ from discord.ext import commands
 @commands.command(help="Displays the URL to register the current guild.")
 async def register(ctx):
     if ctx.guild is None:
-        await ctx.send(f"A siteadmin can register this guild at: {os.getenv('META_SERVICE_BASE_URL', '')}/newguild")
+        await ctx.send(f"A siteadmin can register a guild at: {os.getenv('META_SERVICE_BASE_URL', '')}/newguild")
     else:
         await ctx.send(f"A siteadmin can register this guild at: {os.getenv('META_SERVICE_BASE_URL', '')}/newguild?guildid={ctx.guild.id}")
