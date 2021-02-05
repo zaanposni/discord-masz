f45c2c36cd653f01fb19339b81a804923cd97602
zaanposni <me@zaanposni.com>
2021-02-05 19:32:22 +0100

    added ban and kick command

--- discordbot/src/commands/kick.py
+++ discordbot/src/commands/kick.py
@@ -7,7 +7,7 @@ from discord.ext import commands
 from discord import Embed, Member
 from discord.errors import NotFound
 
-from .checks import registered_guild_with_muted_role_and_admin_or_mod_only
+from .checks import registered_guild_and_admin_or_mod_only
 
 regex = re.compile(r"^[0-9]{18}$")
 
@@ -15,9 +15,9 @@ headers = {
     'Authorization': os.getenv("DISCORD_BOT_TOKEN")
 }
 
-@commands.command(help="Mute a user.")
-@registered_guild_with_muted_role_and_admin_or_mod_only()
-async def mute(ctx, member: Member, *reason):
+@commands.command(help="Kick a user.")
+@registered_guild_and_admin_or_mod_only()
+async def kick(ctx, member: Member, *reason):
     if not len(reason):
         await ctx.send("Please provide a reason.")
         return
@@ -28,25 +28,25 @@ async def mute(ctx, member: Member, *reason):
         "description": reason,
         "modid": ctx.author.id,
         "userid": member.id,
-        "punishment": "Mute",
+        "punishment": "Kick",
         "labels": [],
-        "PunishmentType": 1,
+        "PunishmentType": 2,
         "PunishmentActive": True
     }
 
     r = requests.post(f"http://masz_backend/internalapi/v1/guilds/{ctx.guild.id}/modcases", json=modCase, headers=headers)
 
     if r.status_code == 201:
-        await ctx.send(f"Case #{r.json()['caseid']} created and user muted.\nFollow this link for more information: {os.getenv('META_SERVICE_BASE_URL', 'URL not set.')}")
+        await ctx.send(f"Case #{r.json()['caseid']} created and user kicked.\nFollow this link for more information: {os.getenv('META_SERVICE_BASE_URL', 'URL not set.')}")
     elif r.status_code == 401:
         await ctx.send("You are not allowed to do this.")
     else:
         await ctx.send("Something went wrong.")
 
 
-@mute.error
-async def mute_error(ctx, error):
+@kick.error
+async def kick_error(ctx, error):
     if isinstance(error, commands.BadArgument):
         await ctx.send('I could not find that member...')
     if isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send(r"Please use \>mute @user the reason")
+        await ctx.send(r"Please use \>kick @user the reason")
