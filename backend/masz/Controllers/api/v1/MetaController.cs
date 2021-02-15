0b87b5cbdafba14881b15e816683cdb7f6ce6fd1
zaanposni <me@zaanposni.com>
2021-02-15 17:17:59 +0100

    added update information in navbar

--- backend/masz/Controllers/api/v1/MetaController.cs
+++ backend/masz/Controllers/api/v1/MetaController.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Threading.Tasks;
 using masz.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using RestSharp;
 
 namespace masz.Controllers
 {
@@ -22,10 +24,23 @@ namespace masz.Controllers
         }
 
         [HttpGet("clientid")]
-        public IActionResult Status() {
+        public IActionResult GetClientId() {
             logger.LogInformation($"{HttpContext.Request.Method} {HttpContext.Request.Path} | Incoming request.");
 
             return Ok(new { clientid = config.Value.DiscordClientId });
+        }
+
+        [HttpGet("githubreleases")]
+        public async Task<IActionResult> GetReleases() {
+            logger.LogInformation($"{HttpContext.Request.Method} {HttpContext.Request.Path} | Incoming request.");
+
+            var restClient = new RestClient("https://api.github.com/");
+            var request = new RestRequest(Method.GET);
+            request.Resource = "/repos/zaanposni/discord-masz/releases";
+
+            var response = await restClient.ExecuteAsync(request);
+
+            return Ok(response.Content);
         }
     }
 }
