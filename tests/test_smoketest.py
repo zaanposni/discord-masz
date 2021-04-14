ed8f3f143a807755755fb3888e22d023350f746b
zaanposni <me@zaanposni.com>
2021-04-14 08:26:56 +0200

    fix smoketest

--- tests/test_smoketest.py
+++ tests/test_smoketest.py
@@ -19,11 +19,21 @@ def test_ping_favicon():
     r = requests.get("http://127.0.0.1:5565/favicon.ico")
     assert r.status_code == 200
 
-def test_ping_favicon():
+def test_ping_robots_txt():
+    r = requests.get("http://127.0.0.1:5565/robots.txt")
+    assert r.status_code == 200
+
+def test_ping_patchnotes():
     r = requests.get("http://127.0.0.1:5565/static/patchnotes.json")
     assert r.status_code == 200
     data = r.json()  # should not throw error
     assert isinstance(data, list)
+
+def test_ping_version():
+    r = requests.get("http://127.0.0.1:5565/static/version.json")
+    assert r.status_code == 200
+    data = r.json()  # should not throw error
+    assert isinstance(data, dict)
 
 def test_healthcheck_backend():
     headers = {
