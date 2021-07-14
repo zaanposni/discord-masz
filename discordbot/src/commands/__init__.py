1f3f5d41d0e7201b1ad5b988d737cd18477e5a96
zaanposni <me@zaanposni.com>
2021-07-14 17:18:02 +0200

    view command

--- discordbot/src/commands/__init__.py
+++ discordbot/src/commands/__init__.py
@@ -13,7 +13,8 @@ from .tempmute import tempmute
 from .tempban import tempban
 from .cases import cases
 from .viewg import viewg
+from .view import view
 from .help import help
 
-ALL_COMMANDS = [ version, url, features, register, whois, invite, mute, kick, ban, warn, report, tempmute, tempban, cases, viewg, help ]
+ALL_COMMANDS = [ version, url, features, register, whois, invite, mute, kick, ban, warn, report, tempmute, tempban, cases, viewg, view, help ]
 
--- discordbot/src/commands/view.py
+++ discordbot/src/commands/view.py
@@ -1,15 +1,16 @@
 from discord.ext import commands
 
-from .checks import _is_admin_or_mod
+from .checks import _is_admin_or_mod, registered_guild_only
 from helpers import create_modcase_embed
 from data import get_modcase_by_guild_and_case_id
 
 
 @commands.command(help="View details of a modcase.")
-async def viewg(ctx, guild_id, case_id):
-    if not str(guild_id).isnumeric() or not str(case_id).isnumeric():
+@registered_guild_only()
+async def view(ctx, case_id):
+    if not str(case_id).isnumeric():
         return await ctx.send("Invalid input")
-    modcase = await get_modcase_by_guild_and_case_id(int(guild_id), int(case_id))
+    modcase = await get_modcase_by_guild_and_case_id(int(ctx.guild.id), int(case_id))
     if not modcase:
         return await ctx.send("Not found.")
 
