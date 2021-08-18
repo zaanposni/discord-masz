6db8dcd4938df078adbc53cc875e0ac267de2f30
zaanposni <me@zaanposni.com>
2021-08-18 16:02:46 +0200

    rewrite all checks

--- discordbot/src/commands/checks.py
+++ discordbot/src/commands/checks.py
@@ -34,13 +34,10 @@ async def _is_registered_guild(ctx) -> bool:
     return cfg is not None
 
 
-def guild_only():
-    async def predicate(ctx):
-        if ctx.guild is None:
-            await ctx.send("Only useable in a guild.")
-            return False
-        return True
-    return commands.check(predicate)
+async def guild_only(ctx):
+    if ctx.guild is None:
+        await ctx.send("Only useable in a guild.")
+        raise CheckFailure("Only useable in a guild.")
 
 
 async def registered_guild_only(ctx):
@@ -56,52 +53,46 @@ async def registered_guild_only(ctx):
         raise CheckFailure(f"MASZ is not registered on this guild.")
 
 
-def registered_guild_and_admin_or_mod_only():
-    async def predicate(ctx):
-        if ctx.guild is None:
-            await ctx.send("Only useable in a guild.")
-            return False
+async def registered_guild_and_admin_or_mod_only(ctx):
+    if ctx.guild is None:
+        await ctx.send("Only useable in a guild.")
+        raise CheckFailure(f"Only useable in a guild.")
 
-        if not await _is_registered_guild(ctx):
-            if os.getenv('META_SERVICE_BASE_URL', ''):
-                await ctx.send(f"MASZ is not registered on this guild.\nA siteadmin can register this guild at: {os.getenv('META_SERVICE_BASE_URL', '')}/guilds/new?guildid={ctx.guild.id}")
-            else:
-                await ctx.send(f"MASZ is not registered on this guild.")
-            return False
+    if not await _is_registered_guild(ctx):
+        if os.getenv('META_SERVICE_BASE_URL', ''):
+            await ctx.send(f"MASZ is not registered on this guild.\nA siteadmin can register this guild at: {os.getenv('META_SERVICE_BASE_URL', '')}/guilds/new?guildid={ctx.guild.id}")
+        else:
+            await ctx.send(f"MASZ is not registered on this guild.")
+        raise CheckFailure(f"MASZ is not registered on this guild.")
 
-        if not await _is_admin_or_mod(ctx):
-            await ctx.send("You do not have the permission to use this command.")
-            return False
-        return True
-    return commands.check(predicate)
+    if not await _is_admin_or_mod(ctx):
+        await ctx.send("You do not have the permission to use this command.")
+        raise CheckFailure(f"You do not have the permission to use this command.")
 
 
-def registered_guild_with_muted_role_and_admin_or_mod_only():
-    async def predicate(ctx):
-        if ctx.guild is None:
-            await ctx.send("Only useable in a guild.")
-            return False
+async def registered_guild_with_muted_role_and_admin_or_mod_only(ctx):
+    if ctx.guild is None:
+        await ctx.send("Only useable in a guild.")
+        raise CheckFailure("Only useable in a guild.")
 
-        if not await _is_registered_guild(ctx):
-            if os.getenv('META_SERVICE_BASE_URL', ''):
-                await ctx.send(f"MASZ is not registered on this guild.\nA siteadmin can register this guild at: {os.getenv('META_SERVICE_BASE_URL', '')}/guilds/new?guildid={ctx.guild.id}")
-            else:
-                await ctx.send(f"MASZ is not registered on this guild.")
-            return False
+    if not await _is_registered_guild(ctx):
+        if os.getenv('META_SERVICE_BASE_URL', ''):
+            await ctx.send(f"MASZ is not registered on this guild.\nA siteadmin can register this guild at: {os.getenv('META_SERVICE_BASE_URL', '')}/guilds/new?guildid={ctx.guild.id}")
+        else:
+            await ctx.send(f"MASZ is not registered on this guild.")
+        raise CheckFailure("MASZ is not registered on this guild.")
 
-        if not await _is_admin_or_mod(ctx):
-            await ctx.send("You do not have the permission to use this command.")
-            return False
-        
-        cfg = await get_cached_guild_config(ctx.guild.id)
-        if not cfg["MutedRoles"]:
-            await ctx.send("Muted Roles are not defined.")
-            return False
+    if not await _is_admin_or_mod(ctx):
+        await ctx.send("You do not have the permission to use this command.")
+        raise CheckFailure("You do not have the permission to use this command.")
+    
+    cfg = await get_cached_guild_config(ctx.guild.id)
+    if not cfg["MutedRoles"]:
+        await ctx.send("Muted Roles are not defined.")
+        raise CheckFailure("Muted Roles are not defined.")
 
-        for role in cfg["MutedRoles"].split(","):
-            muted_role = ctx.guild.get_role(int(role))
-            if not (muted_role is not None and ctx.me.top_role > muted_role):
-                await ctx.send("Muted Role is invalid or too high in role hierarchy.")
-                return False
-        return True
-    return commands.check(predicate)
+    for role in cfg["MutedRoles"].split(","):
+        muted_role = ctx.guild.get_role(int(role))
+        if not (muted_role is not None and ctx.me.top_role > muted_role):
+            await ctx.send("Muted Role is invalid or too high in role hierarchy.")
+            raise CheckFailure("Muted Role is invalid or too high in role hierarchy.")
