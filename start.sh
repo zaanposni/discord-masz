534b7a070f918a591b127e9b10cac15ba64e9028
zaanposni <me@zaanposni.com>
2021-09-03 19:32:48 +0200

    remove invitationtracker and discordbot containers

--- start.sh
+++ start.sh
@@ -14,13 +14,9 @@ docker ps -a
 echo "Removing old containers/images/volumes"
 docker container rm masz_nginx
 docker container rm masz_backend
-docker container rm masz_discordbot
-docker container rm masz_invitationtracker
 
 docker image rm discord-masz_nginx
 docker image rm discord-masz_backend
-docker image rm discord-masz_discordbot
-docker image rm discord-masz_invitationtracker
 
 echo "Removed old containers/images/volumes"
 
