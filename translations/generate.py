0590f64ae567bc5acbfc1182cb2a321fa2749a38
zaanposni <me@zaanposni.com>
2021-09-27 16:13:54 +0200

    adjusted translation generator for frontend, added translation to index

--- translations/generate.py
+++ translations/generate.py
@@ -6,6 +6,7 @@ from rich.console import Console
 console = Console()
 
 BACKEND_OUTPUT_PATH = "../backend/masz/Translations/Translation.cs"
+FRONTEND_OUTPUT_PATH = "../nginx/MASZ/src/assets/i18n/"
 
 DEFAULT_ENUM_NAMESPACE = "masz.Enums"
 
@@ -36,8 +37,8 @@ with console.status("[bold green]Generating backend...") as status:
 
     def generate_backend_node(node, prevKeys = ""):
         global TRANSLATION_NODES
-        global BACKEND_STRING
         global TRANSLATION_STATS
+        global BACKEND_STRING
         if "description" not in node:
             for key, value in node.items():
                 generate_backend_node(value, prevKeys + key)
@@ -69,8 +70,8 @@ with console.status("[bold green]Generating backend...") as status:
 
     def generate_backend_enum(node, enum_name):
         global TRANSLATION_NODES
-        global BACKEND_STRING
         global TRANSLATION_STATS
+        global BACKEND_STRING
         console.log(f"Generating {enum_name}...")
         vars = [f"{v} {k}" for k, v in node.get("var_types", dict()).items()]
         generate = f"\t\tpublic string Enum({DEFAULT_ENUM_NAMESPACE}.{enum_name} enumValue) " + "{\n"
@@ -105,6 +106,44 @@ with console.status("[bold green]Generating backend...") as status:
         f.write(BACKEND_STRING.replace("\t", "    "))
 
 console.log("[bright_green]Generated backend[/bright_green].")
+
+def nested_set(dic, keys, value):
+    for key in keys[:-1]:
+        dic = dic.setdefault(key, {})
+    dic[keys[-1]] = value
+
+with console.status("[bold green]Generating frontend...") as status:
+    with open("frontend.json", "r", encoding="utf-8") as f:
+        FRONTEND_DATA = json.load(f)
+    NEW_SPLITTED_DATA = dict()
+
+    def generate_frontend_node(node, prevKeys = list()):
+        global TRANSLATION_NODES
+        global TRANSLATION_STATS
+        global NEW_SPLITTED_DATA
+        if "description" not in node:
+            for key, value in node.items():
+                generate_frontend_node(value, prevKeys + [key])
+        else:
+            console.log(f"Generating {','.join(prevKeys)}...")
+            TRANSLATION_NODES += 1
+            for lang, translation in node.items():
+                if lang.lower() in ["description"]:
+                    continue
+                t = translation.replace('\n', '\\n')
+                TRANSLATION_STATS[lang.lower()] = TRANSLATION_STATS.get(lang.lower(), 0) + 1
+                nested_set(NEW_SPLITTED_DATA, [lang] + prevKeys, t)
+
+    for key, value in FRONTEND_DATA.items():
+        generate_frontend_node(value, [key])
+
+    for lang, value in NEW_SPLITTED_DATA.items():
+        console.log(f"Saving {lang}.json")
+        with open(FRONTEND_OUTPUT_PATH + f"{lang}.json", "w", encoding="utf-8") as f:
+            json.dump(value, f, indent=4, ensure_ascii=False)
+
+console.log("[bright_green]Generated frontend[/bright_green].")
+
 
 with console.status("[bold green]Generating language stats...") as status:
     with open("supported_languages.json", "r", encoding="utf-8") as f:
