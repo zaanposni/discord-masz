1d3a74575cdfe9c3ff4d50564777e99a2a80dc9c
zaanposni <me@zaanposni.com>
2021-10-22 19:11:11 +0200

    renamed warn option in slashcommand

--- backend/masz/Enums/PunishmentTypeEnum.cs
+++ backend/masz/Enums/PunishmentTypeEnum.cs
@@ -1,10 +1,16 @@
+using DSharpPlus.SlashCommands;
+
 namespace masz.Enums
 {
     public enum PunishmentType
     {
+        [ChoiceName("Warn")]
         None,
+        [ChoiceName("Mute")]
         Mute,
+        [ChoiceName("Kick")]
         Kick,
+        [ChoiceName("Ban")]
         Ban
     }
 }
