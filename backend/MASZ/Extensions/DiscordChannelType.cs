f8835f20cdf38764085c30f4fcede5e04640ac3b
FeroxFoxxo <frostrixz@gmail.com>
2021-12-17 23:49:25 +1300

    Fixed thread channel type switching.

--- backend/MASZ/Extensions/DiscordChannelType.cs
+++ backend/MASZ/Extensions/DiscordChannelType.cs
@@ -9,13 +9,20 @@ namespace MASZ.Extensions
         {
             switch (channel.GetType().GetInterfaces())
             {
+                case Type[] chnl when chnl.Contains(typeof(IThreadChannel)):
+                    return (channel as IThreadChannel).Type switch
+                    {
+                        ThreadType.PublicThread => ChannelType.PublicThread,
+                        ThreadType.PrivateThread => ChannelType.PrivateThread,
+                        ThreadType.NewsThread => ChannelType.NewsThread,
+                        _ => throw new NotImplementedException(),
+                    };
                 case Type[] chnl when chnl.Contains(typeof(ITextChannel)): return ChannelType.Text;
                 case Type[] chnl when chnl.Contains(typeof(IDMChannel)): return ChannelType.DM;
                 case Type[] chnl when chnl.Contains(typeof(IVoiceChannel)): return ChannelType.Voice;
                 case Type[] chnl when chnl.Contains(typeof(IGroupChannel)): return ChannelType.Group;
                 case Type[] chnl when chnl.Contains(typeof(ICategoryChannel)): return ChannelType.Category;
                 case Type[] chnl when chnl.Contains(typeof(INewsChannel)): return ChannelType.News;
-                case Type[] chnl when chnl.Contains(typeof(IThreadChannel)): return ChannelType.PublicThread;
                 case Type[] chnl when chnl.Contains(typeof(IStageChannel)): return ChannelType.Stage;
                 default: throw new NotImplementedException();
             };
