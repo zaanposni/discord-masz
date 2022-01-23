61c00c46cf7abe6311c59ee005c09cf46740566b
zaanposni <me@zaanposni.com>
2022-01-23 14:09:30 +0100

    fix auditlog webhook regex validator

--- backend/MASZ/Dtos/AppSettings/SettingsAppSettingsForPutDto.cs
+++ backend/MASZ/Dtos/AppSettings/SettingsAppSettingsForPutDto.cs
@@ -7,7 +7,7 @@ namespace MASZ.Dtos.AppSettings
     {
         [Required(ErrorMessage = "DefaultLanguage field is required")]
         public Language DefaultLanguage { get; set; }
-        [RegularExpression("https://discord(app)?.com/api/webhooks/[0-9]+/[A-Za-z0-9]+", ErrorMessage = "Must be a valid url")]
+        [RegularExpression("https://discord(app)?.com/api/webhooks/[0-9]+/.+", ErrorMessage = "Must be a valid url")]
         public string? AuditLogWebhookURL { get; set; }
         [Required(ErrorMessage = "PublicFileMode field is required")]
         public bool PublicFileMode { get; set; }
