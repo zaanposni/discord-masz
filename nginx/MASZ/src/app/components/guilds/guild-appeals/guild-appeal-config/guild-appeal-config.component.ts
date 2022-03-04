6e14f6c96ccac07848769ad888fd627f9298e219
zaanposni <me@zaanposni.com>
2022-03-04 19:25:25 +0100

    .

--- nginx/MASZ/src/app/components/guilds/guild-appeals/guild-appeal-config/guild-appeal-config.component.ts
+++ nginx/MASZ/src/app/components/guilds/guild-appeals/guild-appeal-config/guild-appeal-config.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { ToastrService } from 'ngx-toastr';
+import { ContentLoading } from 'src/app/models/ContentLoading';
+import { IAppealStructure } from 'src/app/models/IAppealStructure';
+import { ApiService } from 'src/app/services/api.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { EnumManagerService } from 'src/app/services/enum-manager.service';
+import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-guild-appeal-config',
@@ -7,9 +18,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GuildAppealConfigComponent implements OnInit {
 
-  constructor() { }
+  public guildId!: string;
+  public newQuestionFormGroup!: FormGroup;
+  maxLength4096 = { length: 4096 };
+
+  public questions: ContentLoading<IAppealStructure[]> = { content: [], loading: true };
+
+  constructor(private route: ActivatedRoute, private auth: AuthService, private toastr: ToastrService, private api: ApiService, public router: Router, private _formBuilder: FormBuilder, private dialog: MatDialog, private enumManager: EnumManagerService, private translator: TranslateService) { }
 
   ngOnInit(): void {
+    this.guildId = this.route.snapshot.paramMap.get('guildid') as string;
+
+    this.newQuestionFormGroup = this._formBuilder.group({
+      question: ['', [ Validators.required, Validators.maxLength(4096) ]]
+    });
+
+    this.reload();
   }
 
+  reload() {
+    this.api.getSimpleData(`/guilds/${this.guildId}/appealstructures`).subscribe((data: IAppealStructure[]) => {
+      this.questions.content = data;
+      this.questions.loading = false;
+    });
+  }
+
+  saveNewQuestion() {
+    const data = {
+      question: this.newQuestionFormGroup.value.question,
+      sortOrder: Math.max(...this.questions.content ? this.questions.content.map(x => x.sortOrder) : [0], 0) + 1,
+    };
+
+    this.api.postSimpleData(`/guilds/${this.guildId}/appealstructures`, data).subscribe((data: IAppealStructure) => {
+      if (this.questions.content) {
+        this.questions.content.push(data);
+      }
+      this.newQuestionFormGroup.reset();
+      Object.keys(this.newQuestionFormGroup.controls).forEach(key =>{
+        this.newQuestionFormGroup.controls[key].setErrors(null)
+      });
+    }, error => {
+      console.error(error);
+      this.toastr.error("oh no");
+    });
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    console.log(event);
+    // moveItemInArray(this.questions, event.previousIndex, event.currentIndex);
+  }
+
+  get newQuestion() { return this.newQuestionFormGroup.get('question'); }
 }
