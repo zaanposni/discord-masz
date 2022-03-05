f3d7dd09c620afaf837b7371041ee72cc51652c6
zaanposni <me@zaanposni.com>
2022-03-05 19:29:36 +0100

    guild appeal card

--- nginx/MASZ/src/app/components/guilds/guild-appeals/guild-appeals-card/guild-appeals-card.component.ts
+++ nginx/MASZ/src/app/components/guilds/guild-appeals/guild-appeals-card/guild-appeals-card.component.ts
@@ -1,5 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { APIEnumTypes } from 'src/app/models/APIEmumTypes';
+import { APIEnum } from 'src/app/models/APIEnum';
+import { AppealStatus } from 'src/app/models/AppealStatus';
 import { IAppealView } from 'src/app/models/IAppealView';
+import { ApiService } from 'src/app/services/api.service';
+import { EnumManagerService } from 'src/app/services/enum-manager.service';
 
 @Component({
   selector: 'app-guild-appeals-card',
@@ -8,10 +13,19 @@ import { IAppealView } from 'src/app/models/IAppealView';
 })
 export class GuildAppealsCardComponent implements OnInit {
 
+  AppealStatus = AppealStatus;
   @Input() entry!: IAppealView;
-  constructor() { }
+  public status: string = "Unknown";
+  questionsAnswered: { count: number } = { count: 0 };
+
+  constructor(private enumManager: EnumManagerService) { }
 
   ngOnInit(): void {
-  }
+    this.enumManager.getEnum(APIEnumTypes.APPEALSTATUS).subscribe((data: APIEnum[]) => {
+      this.status = data.find(x => x.key === this.entry.status)?.value ?? "Unknown";
+    });
 
+    console.log(this.entry.answers);
+    this.questionsAnswered = { count: this.entry.answers.filter(x => x.answer?.trim()).length };
+  }
 }
