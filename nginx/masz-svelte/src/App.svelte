baed96781a274e90b4b7a8cb205296dd0f867d99
zaanposni <me@zaanposni.com>
2022-03-13 18:42:04 +0100

    initial svelte setup

---
+++ nginx/masz-svelte/src/App.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { Router } from "@roxi/routify";
+	import { routes } from "../.routify/routes";
+  	import { Button } from 'attractions';
+
+
+	export let name: string;
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<Router {routes} />
+	<Button filled class="mb-3">click me</Button>
+	<i class="material-icons">home</i>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
