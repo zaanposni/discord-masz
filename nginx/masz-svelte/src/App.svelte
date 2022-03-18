07bef137cbc7b9fa69b3397aab93f22667376228
zaanposni <me@zaanposni.com>
2022-03-18 20:09:44 +0100

    navbar

--- nginx/masz-svelte/src/App.svelte
+++ nginx/masz-svelte/src/App.svelte
@@ -1,29 +1,95 @@
 <script lang="ts">
-	import { Router } from "@roxi/routify";
-	import { routes } from "../.routify/routes";
+  import { Router } from "@roxi/routify";
+  import { routes } from "../.routify/routes";
+  import "carbon-components-svelte/css/all.css";
 
-	import "carbon-components-svelte/css/all.css";
+  import {
+    Header,
+    SideNav,
+    SideNavItems,
+    SideNavMenu,
+    SideNavMenuItem,
+    SideNavLink,
+    SideNavDivider,
+    SkipToContent,
+    Content,
+    HeaderUtilities,
+    HeaderGlobalAction,
+    HeaderPanelLink,
+    HeaderPanelLinks,
+    HeaderAction,
+    HeaderPanelDivider,
+  } from "carbon-components-svelte";
+  import Fade16 from "carbon-icons-svelte/lib/Fade16";
+  import SettingsAdjust20 from "carbon-icons-svelte/lib/SettingsAdjust20";
+  import UserAvatarFilledAlt20 from "carbon-icons-svelte/lib/UserAvatarFilledAlt20";
 
-	import { Accordion, AccordionItem } from "carbon-components-svelte";
-	import Add32 from "carbon-icons-svelte/lib/Add32";
+  import Theme from "./components/nav/theme.svelte";
 
-	import Theme from "./components/nav/theme.svelte";
+  let isSideNavOpen = false;
+  let isOpen1 = false;
+  let isOpen2 = false;
 </script>
 
-<style global lang="postcss">
-    @tailwind base;
-    @tailwind components;
-    @tailwind utilities;
-</style>
+<Header company="MASZ" platformName="v3.0" bind:isSideNavOpen>
+  <svelte:fragment slot="skip-to-content">
+    <SkipToContent />
+  </svelte:fragment>
+  <HeaderUtilities>
+    <HeaderGlobalAction aria-label="Settings" icon={SettingsAdjust20} />
+    <HeaderAction bind:isOpen={isOpen1} icon={UserAvatarFilledAlt20} closeIcon={UserAvatarFilledAlt20}>
+      <HeaderPanelLinks>
+        <HeaderPanelDivider>Switcher subject 1</HeaderPanelDivider>
+        <HeaderPanelLink>Switcher item 1</HeaderPanelLink>
+        <HeaderPanelLink>Switcher item 2</HeaderPanelLink>
+        <HeaderPanelLink>Switcher item 3</HeaderPanelLink>
+        <HeaderPanelLink>Switcher item 4</HeaderPanelLink>
+        <HeaderPanelDivider>Switcher subject 2</HeaderPanelDivider>
+        <HeaderPanelLink>Switcher item 1</HeaderPanelLink>
+        <HeaderPanelLink>Switcher item 2</HeaderPanelLink>
+        <HeaderPanelDivider>Switcher subject 3</HeaderPanelDivider>
+        <HeaderPanelLink>Switcher item 1</HeaderPanelLink>
+      </HeaderPanelLinks>
+    </HeaderAction>
+    <HeaderAction bind:isOpen={isOpen2}>
+      <HeaderPanelLinks>
+        <HeaderPanelDivider>Switcher subject 1</HeaderPanelDivider>
+        <HeaderPanelLink>Switcher item 1</HeaderPanelLink>
+        <HeaderPanelDivider>Switcher subject 2</HeaderPanelDivider>
+        <HeaderPanelLink>Switcher item 1</HeaderPanelLink>
+        <HeaderPanelLink>Switcher item 2</HeaderPanelLink>
+        <HeaderPanelLink>Switcher item 3</HeaderPanelLink>
+        <HeaderPanelLink>Switcher item 4</HeaderPanelLink>
+        <HeaderPanelLink>Switcher item 5</HeaderPanelLink>
+      </HeaderPanelLinks>
+    </HeaderAction>
+  </HeaderUtilities>
+</Header>
 
-<main>
-	<Theme />
-	<Router {routes} />
-	<i class="material-icons">home</i>
-	<Add32 title="Add" class="my-5" />
-	<Accordion>
-	  <AccordionItem title="Section 1" open> Content 1 </AccordionItem>
-	  <AccordionItem title="Section 2"> Content 2 </AccordionItem>
-	  <AccordionItem title="Section 3"> Content 3 </AccordionItem>
-	</Accordion>
-</main>
+<SideNav bind:isOpen={isSideNavOpen} rail>
+  <SideNavItems>
+    <SideNavLink icon={Fade16} text="Link 1" href="/" isSelected />
+    <SideNavLink icon={Fade16} text="Link 2" href="/" />
+    <SideNavLink icon={Fade16} text="Link 3" href="/" />
+    <SideNavMenu icon={Fade16} text="Menu">
+      <SideNavMenuItem href="/" text="Link 1" />
+      <SideNavMenuItem href="/" text="Link 2" />
+      <SideNavMenuItem href="/" text="Link 3" />
+    </SideNavMenu>
+    <SideNavDivider />
+    <SideNavLink icon={Fade16} text="Link 4" href="/" />
+  </SideNavItems>
+</SideNav>
+
+<Content>
+  <main>
+    <Theme />
+    <Router {routes} />
+  </main>
+</Content>
+
+<style global lang="postcss">
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
---
+++ nginx/masz-svelte/src/core/Autocomplete.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+	import { ComboBox } from "carbon-components-svelte";
+
+	let warnText = "";
+
+	let selectedId;
+	let value;
+	let items = [
+	  { id: 1, text: "Email" },
+	  { id: 2, text: "Fax" },
+	];
+
+	function shouldFilterItem(item, value) {
+	  	if (!value) return true;
+	  	return item.text.toLowerCase().includes(value.toLowerCase());
+	}
+
+	let debounce;
+	function onKeyDown(event) {
+		if (event.key.length === 1) {
+			if (debounce) {
+				clearTimeout(debounce);
+			}
+			debounce = setTimeout(() => {
+				const index = items.findIndex(x => x.id === 0);
+				if (index !== -1) {
+					items[index].text = value;
+				} else {
+					items.unshift({ id: 0, text: value });
+				}
+			}, 300);
+		}
+	}
+
+	function onSelect() {
+		// if selected id is 0
+		if (selectedId === 0) {
+			warnText = "This is a custom item.";
+		}
+	}
+
+	function onClear() {
+		warnText = "";
+		items = items.filter(x => x.id !== 0);
+	}
+
+	$: value ? undefined: onClear();
+</script>
+
+<ComboBox
+	titleText="Contact"
+	placeholder="Select contact method"
+	{items}
+	{warnText}
+	warn={!!warnText}
+	{shouldFilterItem}
+	bind:selectedId
+	bind:value
+	on:select={onSelect}
+	on:keydown={onKeyDown}
+/>
+{value}
+{selectedId}
