3da0c06f9eb4bc3e8e0ed2f66ac6db94c19a2027
zaanposni <me@zaanposni.com>
2022-03-20 12:05:04 +0100

    language select

--- nginx/masz-svelte/src/components/nav/usersettings.svelte
+++ nginx/masz-svelte/src/components/nav/usersettings.svelte
@@ -2,8 +2,11 @@
     import { Modal } from "carbon-components-svelte";
     import { showUserSettings } from "./store";
     import { Theme } from "carbon-components-svelte";
-    import { LOCAL_STORAGE_KEY_THEME } from "../../config";
+    import { LANGUAGES, LOCAL_STORAGE_KEY_THEME } from "../../config";
     import { currentTheme } from "../../stores/theme";
+    import { Select, SelectItem } from "carbon-components-svelte";
+    import { locale } from "svelte-i18n";
+    import { currentLanguage } from "../../stores/currentLanguage";
 
     const themeSettings = {
         themes: ["white", "g10", "g90", "g100"],
@@ -13,6 +16,14 @@
     function onModalClose() {
         currentTheme.set(localStorage.getItem(LOCAL_STORAGE_KEY_THEME));
         showUserSettings.set(false);
+    }
+
+    function onLanguageSelect({detail}) {
+        const language = LANGUAGES.find(l => l.language === detail);
+        if (language) {
+            locale.set(language.language);
+            currentLanguage.set(language);
+        }
     }
 </script>
 
@@ -24,4 +35,9 @@
     on:close={onModalClose}
     on:submit={onModalClose}>
     <Theme select={themeSettings} render="select" persist persistKey={LOCAL_STORAGE_KEY_THEME} />
+    <Select class="mt-4" labelText="Language" on:change={onLanguageSelect}>
+        {#each LANGUAGES as language}
+            <SelectItem value={language.language} text={language.displayName} />
+        {/each}
+    </Select>
 </Modal>
