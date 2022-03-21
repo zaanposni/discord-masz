529e3f6f1cd837c1e2950e6f07168f09d1f47872
zaanposni <me@zaanposni.com>
2022-03-21 20:09:22 +0100

    admin "authguard"

---
+++ nginx/masz-svelte/src/pages/admin/_layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import { goto } from "@roxi/routify";
+    import { authUser } from "../../stores/auth";
+
+    let checkForAdmin = true;
+    $: if ($authUser && checkForAdmin) {
+        if (!$authUser?.isAdmin) {
+            $goto("/");
+        } else {
+            checkForAdmin = false;
+        }
+    }
+</script>
+
+<slot />
