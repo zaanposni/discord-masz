70cbdabdf1b031c3933546c09b6fe07c4797477f
zaanposni <me@zaanposni.com>
2022-04-17 09:54:11 +0200

    fix transition routing error

--- nginx/masz-svelte/src/components/guilds/cases/CaseTable.svelte
+++ nginx/masz-svelte/src/components/guilds/cases/CaseTable.svelte
@@ -181,7 +181,7 @@
             <div
                 class="grid gap-1 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-6 3xl:grid-cols-12 mt-4"
                 id="filter-parent"
-                transition:slide>
+                transition:slide|local>
                 <div>
                     <MultiSelect
                         spellcheck="false"
