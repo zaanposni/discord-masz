834c335440f1765c164612d84f2b420ef711a9f7
zaanposni <me@zaanposni.com>
2022-05-26 09:24:36 +0200

    improve responsive design for patchnotes

--- nginx/masz-svelte/src/components/nav/patchnotes/Patchnotes.svelte
+++ nginx/masz-svelte/src/components/nav/patchnotes/Patchnotes.svelte
@@ -4,6 +4,7 @@
     import moment from "moment";
     import type { Writable } from "svelte/store";
     import { writable } from "svelte/store";
+    import MediaQuery from "../../../core/MediaQuery.svelte";
     import type { IPatchnote, IPatchnoteSeperator } from "../../../models/IPatchnote";
     import API from "../../../services/api/api";
     import { currentLanguage } from "../../../stores/currentLanguage";
@@ -40,121 +41,129 @@
     }
 </style>
 
-<div class="flex flex-row items-center justify-center w-full">
-    <div class="grid grid-cols-8 gap-0 w-full lg:w-1/2">
-        {#each $patchnotes as patchnote}
-            {#if patchnote.text}
-                <div />
-                <div
-                    class="flex flex-row justify-center border-solid border-2 col-span-2 px-1 py-1 mx-4"
-                    style="border-color: #161616; color: var(--cds-text-05, #6f6f6f)">
-                    <div class="mx-auto">
-                        {patchnote.text}
+<MediaQuery query="(min-width: 1024px)" let:matches>
+    <div class="flex flex-row items-center justify-center w-full">
+        <div class="grid grid-cols-8 gap-0 w-full lg:w-1/2">
+            {#each $patchnotes as patchnote}
+                {#if patchnote.text}
+                    {#if matches}
+                        <div />
+                    {/if}
+                    <div
+                        class="flex flex-row justify-center border-solid border-2 col-span-2 px-1 py-1" class:mx-4={matches}
+                        style="border-color: #161616; color: var(--cds-text-05, #6f6f6f)">
+                        <div class="">
+                            {patchnote.text}
+                        </div>
                     </div>
-                </div>
-                <div class="col-span-5" />
-            {:else}
-                <div
-                    class="border-solid border-r-2 col-span-2 pb-8 pr-4"
-                    style="border-color: #161616; color: var(--cds-text-05, #6f6f6f); padding-top: 2.6rem">
-                    {patchnote.released_at != undefined
-                        ? moment(patchnote.released_at).format($currentLanguage?.momentDateFormat ?? "DD/MM/YYYY")
-                        : "Unknown"}
-                </div>
-                <div class="py-8 pl-4 col-span-6">
-                    <div class="text-xl font-bold mb-4">{patchnote.version}</div>
-                    {#if patchnote.contributors}
-                        <div class="flex flex-row flex-wrap mb-4 contributors">
-                            {#each patchnote.contributors as contributor}
-                                <a class="-mr-6" href={contributor.link} target="_blank">
-                                    <img class="w-12 h-12 rounded-full" src={contributor.icon} alt={contributor.name} />
-                                </a>
+                    <div class="col-span-5" class:col-span-6={!matches} />
+                {:else}
+                    {#if matches}
+                        <div
+                            class="col-span-2 pb-8 pr-4"
+                            style="color: var(--cds-text-05, #6f6f6f); padding-top: 2.6rem">
+                            {patchnote.released_at != undefined
+                                ? moment(patchnote.released_at).format($currentLanguage?.momentDateFormat ?? "DD/MM/YYYY")
+                                : "Unknown"}
+                        </div>
+                    {:else}
+                        <div />
+                    {/if}
+                    <div class="border-solid border-l-2 py-8 pl-4 col-span-6" class:col-span-7={!matches} style="border-color: #161616">
+                        <div class="text-xl font-bold mb-4">{patchnote.version}</div>
+                        {#if patchnote.contributors}
+                            <div class="flex flex-row flex-wrap mb-4 contributors">
+                                {#each patchnote.contributors as contributor}
+                                    <a class="-mr-6" href={contributor.link} target="_blank">
+                                        <img class="w-12 h-12 rounded-full" src={contributor.icon} alt={contributor.name} />
+                                    </a>
+                                {/each}
+                            </div>
+                        {/if}
+                        <div class="grid grid-cols-8 gap-x-0 gap-y-4">
+                            {#each patchnote?.breaking ?? [] as entry}
+                                <div class="flex flex-row col-span-7">
+                                    <div>
+                                        <WarningFilled24 style="fill: var(--cds-support-error-inverse)" />
+                                    </div>
+                                    <div class="flex flex-col px-4">
+                                        <div class="text-lg font-semibold">{entry.title}</div>
+                                        <div>{entry.description}</div>
+                                    </div>
+                                </div>
+                                <div class="pt-1">
+                                    {#if entry.link}
+                                        <OutboundLink href={entry.link}>Link</OutboundLink>
+                                    {/if}
+                                </div>
+                            {/each}
+                            {#each patchnote?.features ?? [] as entry}
+                                <div class="flex flex-row col-span-7">
+                                    <div>
+                                        <StarFilled24 style="fill: var(--cds-support-success-inverse)" />
+                                    </div>
+                                    <div class="flex flex-col px-4">
+                                        <div class="text-lg font-semibold">{entry.title}</div>
+                                        <div>{entry.description}</div>
+                                    </div>
+                                </div>
+                                <div class="pt-1">
+                                    {#if entry.link}
+                                        <OutboundLink href={entry.link}>Link</OutboundLink>
+                                    {/if}
+                                </div>
+                            {/each}
+                            {#each patchnote?.fixes ?? [] as entry}
+                                <div class="flex flex-row col-span-7">
+                                    <div>
+                                        <CheckmarkFilled24 style="fill: var(--cds-support-warning-inverse)" />
+                                    </div>
+                                    <div class="flex flex-col px-4">
+                                        <div class="text-lg font-semibold">{entry.title}</div>
+                                        <div>{entry.description}</div>
+                                    </div>
+                                </div>
+                                <div class="pt-1">
+                                    {#if entry.link}
+                                        <OutboundLink href={entry.link}>Link</OutboundLink>
+                                    {/if}
+                                </div>
+                            {/each}
+                            {#each patchnote?.technical ?? [] as entry}
+                                <div class="flex flex-row col-span-7">
+                                    <div>
+                                        <LogoGithub24 style="fill: var(--cds-support-info)" />
+                                    </div>
+                                    <div class="flex flex-col px-4">
+                                        <div class="text-lg font-semibold">{entry.title}</div>
+                                        <div>{entry.description}</div>
+                                    </div>
+                                </div>
+                                <div class="pt-1">
+                                    {#if entry.link}
+                                        <OutboundLink href={entry.link}>Link</OutboundLink>
+                                    {/if}
+                                </div>
+                            {/each}
+                            {#each patchnote?.changes ?? [] as entry}
+                                <div class="flex flex-row col-span-7">
+                                    <div>
+                                        <CircleSolid24 style="fill: #161616" />
+                                    </div>
+                                    <div class="flex flex-col px-4">
+                                        <div>{entry}</div>
+                                    </div>
+                                </div>
+                                <div class="pt-1">
+                                    {#if entry.link}
+                                        <OutboundLink href={entry.link}>Link</OutboundLink>
+                                    {/if}
+                                </div>
                             {/each}
                         </div>
-                    {/if}
-                    <div class="grid grid-cols-8 gap-x-0 gap-y-4">
-                        {#each patchnote?.breaking ?? [] as entry}
-                            <div class="flex flex-row col-span-7">
-                                <div>
-                                    <WarningFilled24 style="fill: var(--cds-support-error-inverse)" />
-                                </div>
-                                <div class="flex flex-col px-4">
-                                    <div class="text-lg font-semibold">{entry.title}</div>
-                                    <div>{entry.description}</div>
-                                </div>
-                            </div>
-                            <div class="pt-1">
-                                {#if entry.link}
-                                    <OutboundLink href={entry.link}>Link</OutboundLink>
-                                {/if}
-                            </div>
-                        {/each}
-                        {#each patchnote?.features ?? [] as entry}
-                            <div class="flex flex-row col-span-7">
-                                <div>
-                                    <StarFilled24 style="fill: var(--cds-support-success-inverse)" />
-                                </div>
-                                <div class="flex flex-col px-4">
-                                    <div class="text-lg font-semibold">{entry.title}</div>
-                                    <div>{entry.description}</div>
-                                </div>
-                            </div>
-                            <div class="pt-1">
-                                {#if entry.link}
-                                    <OutboundLink href={entry.link}>Link</OutboundLink>
-                                {/if}
-                            </div>
-                        {/each}
-                        {#each patchnote?.fixes ?? [] as entry}
-                            <div class="flex flex-row col-span-7">
-                                <div>
-                                    <CheckmarkFilled24 style="fill: var(--cds-support-warning-inverse)" />
-                                </div>
-                                <div class="flex flex-col px-4">
-                                    <div class="text-lg font-semibold">{entry.title}</div>
-                                    <div>{entry.description}</div>
-                                </div>
-                            </div>
-                            <div class="pt-1">
-                                {#if entry.link}
-                                    <OutboundLink href={entry.link}>Link</OutboundLink>
-                                {/if}
-                            </div>
-                        {/each}
-                        {#each patchnote?.technical ?? [] as entry}
-                            <div class="flex flex-row col-span-7">
-                                <div>
-                                    <LogoGithub24 style="fill: var(--cds-support-info)" />
-                                </div>
-                                <div class="flex flex-col px-4">
-                                    <div class="text-lg font-semibold">{entry.title}</div>
-                                    <div>{entry.description}</div>
-                                </div>
-                            </div>
-                            <div class="pt-1">
-                                {#if entry.link}
-                                    <OutboundLink href={entry.link}>Link</OutboundLink>
-                                {/if}
-                            </div>
-                        {/each}
-                        {#each patchnote?.changes ?? [] as entry}
-                            <div class="flex flex-row col-span-7">
-                                <div>
-                                    <CircleSolid24 style="fill: #161616" />
-                                </div>
-                                <div class="flex flex-col px-4">
-                                    <div>{entry}</div>
-                                </div>
-                            </div>
-                            <div class="pt-1">
-                                {#if entry.link}
-                                    <OutboundLink href={entry.link}>Link</OutboundLink>
-                                {/if}
-                            </div>
-                        {/each}
                     </div>
-                </div>
-            {/if}
-        {/each}
+                {/if}
+            {/each}
+        </div>
     </div>
-</div>
+</MediaQuery>
