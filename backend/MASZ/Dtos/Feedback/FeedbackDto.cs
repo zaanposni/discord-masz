2abcce298b7187169862c2de45a135c33b6b2abc
zaanposni <me@zaanposni.com>
2022-05-28 20:44:46 +0200

    feedback endpoint

---
+++ backend/MASZ/Dtos/Feedback/FeedbackDto.cs
@@ -0,0 +1,10 @@
+namespace MASZ.Dtos
+{
+    public class FeedbackDto
+    {
+        public int Rating { get; set; }
+        public string Comment { get; set; }
+        public string Remote { get; set; }
+        public string User { get; set; }
+    }
+}
