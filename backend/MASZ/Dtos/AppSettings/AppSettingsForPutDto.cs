c18ac4e2c29662ffcd13d030face6616d00a873d
zaanposni <me@zaanposni.com>
2022-05-28 10:00:13 +0200

    one put endpoint for all appsettings

--- backend/MASZ/Dtos/AppSettings/AppSettingsForPutDto.cs
+++ backend/MASZ/Dtos/AppSettings/AppSettingsForPutDto.cs
@@ -3,8 +3,14 @@ using System.ComponentModel.DataAnnotations;
 
 namespace MASZ.Dtos.AppSettings
 {
-    public class SettingsAppSettingsForPutDto
+    public class AppSettingsForPutDto
     {
+        [Required(ErrorMessage = "EmbedTitle field is required")]
+        [MaxLength(256)]
+        public string EmbedTitle { get; set; }
+        [MaxLength(4096)]
+        public string EmbedContent { get; set; }
+        public bool EmbedShowIcon { get; set; } = false;
         [Required(ErrorMessage = "DefaultLanguage field is required")]
         public Language DefaultLanguage { get; set; }
         [RegularExpression("https://discord(app)?.com/api/webhooks/[0-9]+/.+", ErrorMessage = "Must be a valid url")]
