a52c299f8ca02ed3427c16d8e3fe07a22af309f1
zaanposni <me@zaanposni.com>
2022-06-26 22:10:57 +0200

    .

--- backend/MASZ/InviteTracking/InviteTracker.cs
+++ backend/MASZ/InviteTracking/InviteTracker.cs
@@ -8,6 +8,17 @@ namespace MASZ.InviteTracking
         {
             if (GuildInvites.ContainsKey(guildId))
             {
+                Console.WriteLine("current invites");
+                foreach (var invite in currentInvites)
+                {
+                    Console.WriteLine($"{invite.Code} {invite.Uses} {invite.MaxUses} {invite.ExpiresAt}");
+                }
+                Console.WriteLine("saved invites");
+                foreach (var invite in GuildInvites[guildId])
+                {
+                    Console.WriteLine($"{invite.Code} {invite.Uses} {invite.MaxUses} {invite.ExpiresAt}");
+                }
+
                 List<TrackedInvite> invites = GuildInvites[guildId];
                 List<TrackedInvite> changedInvites = invites.Where(x =>
                     (currentInvites.Find(c => c.Code == x.Code) != null && x.HasNewUses(currentInvites.Find(c => c.Code == x.Code).Uses)) ||  // where invite is in current invites and has new uses
