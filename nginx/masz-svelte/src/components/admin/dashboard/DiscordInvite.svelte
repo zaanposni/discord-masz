5e6bb0fe9b3a2d799110bcfbaa059ed71cefbe47
zaanposni <me@zaanposni.com>
2022-12-22 09:12:32 +0100

    added a discord invite link to the admin dashboard

---
+++ nginx/masz-svelte/src/components/admin/dashboard/DiscordInvite.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import DashboardWidget from "../../../core/dashboard/DashboardWidget.svelte";
+    import { WidgetState } from "../../../core/dashboard/WidgetState";
+    import type { IDashboardItem } from "../../../models/IDashboardItem";
+    import { _ } from "svelte-i18n";
+
+    export let dashboardItem: IDashboardItem;
+</script>
+
+<DashboardWidget
+    title={$_("widgets.admindiscordinvite.title")}
+    mode={dashboardItem.mode}
+    state={WidgetState.Normal}>
+    <div class="dash-widget-list-border flex flex-row items-center py-2">
+        <div class="grow">{$_("widgets.admindiscordinvite.message")}</div>
+    </div>
+    <div class="dash-widget-list-border flex flex-row items-center py-2">
+        <a href="https://discord.gg/5zjpzw6h3S" target="_blank">
+            https://discord.gg/5zjpzw6h3S
+        </a>
+    </div>
+</DashboardWidget>
