8aa3e6930593c86f2c6b95e3a75c3c86cce70bd9
Simply-Cods <79521176+Simply-Cods@users.noreply.github.com>
2022-12-22 15:53:45 +0100

    More fields in VerifiedEvidence

--- backend/MASZ/Models/Database/VerifiedEvidence.cs
+++ backend/MASZ/Models/Database/VerifiedEvidence.cs
@@ -9,6 +9,16 @@ namespace MASZ.Models.Database
         public ulong GuildId { get; set; }
         public ulong MessageId { get; set; }
         public string ReportedContent { get; set; }
+        public ulong ReporterUserId { get; set; }
+        public string ReporterUsername { get; set; }
+        public string? ReporterNickname { get; set; }
+        public int ReporterDiscriminator { get; set; }
+        public ulong ReportedUserId { get; set; }
+        public string ReportedUsername { get; set; }
+        public string? ReportedNickname { get; set; }
+        public int ReportedDiscriminator { get; set; }
+        public DateTime SentAt { get; set; }
+        public DateTime ReportedAt { get; set; }
         public ICollection<ModCaseEvidenceMapping> EvidenceMappings { get; set; }
     }
 }
