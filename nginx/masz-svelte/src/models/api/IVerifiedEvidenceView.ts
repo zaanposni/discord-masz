a1f4503f08b311c4a05bfdc5136f1a9aae2b44fc
Simply-Cods <79521176+Simply-Cods@users.noreply.github.com>
2022-12-23 16:19:56 +0100

    fix: IVerifiedEvidenceView self-reference

--- nginx/masz-svelte/src/models/api/IVerifiedEvidenceView.ts
+++ nginx/masz-svelte/src/models/api/IVerifiedEvidenceView.ts
@@ -1,8 +1,9 @@
 import type { IDiscordUser } from "../discord/IDiscordUser";
 import type { ICase } from "./ICase";
+import type { IVerifiedEvidence } from "./IVerifiedEvidence";
 
 export interface IVerifiedEvidenceView {
-    evidence: IVerifiedEvidenceView;
+    evidence: IVerifiedEvidence;
     reporter: IDiscordUser;
     reported: IDiscordUser;
     linkedCases: ICase[];
