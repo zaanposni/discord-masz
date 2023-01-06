f4fd3c958b4ddc440168a102853295edbc769b56
Simply-Cods <79521176+Simply-Cods@users.noreply.github.com>
2023-01-06 21:48:19 +0100

    remove notification on evidence creation

--- backend/MASZ/Commands/CreateEvidenceCommand.cs
+++ backend/MASZ/Commands/CreateEvidenceCommand.cs
@@ -3,7 +3,6 @@ using Discord.Interactions;
 using MASZ.Attributes;
 using MASZ.Enums;
 using MASZ.Extensions;
-using MASZ.Models;
 using MASZ.Models.Database;
 using MASZ.Repositories;
 using MASZ.Services;
@@ -43,20 +42,6 @@ namespace MASZ.Commands
                 Logger.LogError(e, "Failed to save evidence in database");
                 await Context.Interaction.RespondAsync(Translator.T().CmdEvidenceFailed(), ephemeral: true);
                 return;
-            }
-
-            GuildConfig config = await GuildConfigRepository.CreateDefault(ServiceProvider).GetGuildConfig(Context.Guild.Id);
-
-            if(!string.IsNullOrEmpty(config.ModInternalNotificationWebhook))
-            {
-                try
-                {
-                    // TODO: proper notification
-                    await DiscordAPI.ExecuteWebhook(config.ModInternalNotificationWebhook, null, "created evidence TODO: change this", AllowedMentions.None);
-                } catch (Exception e) 
-                {
-                    Logger.LogError(e, "Failed to send internal notification on evidence creation");
-                }
             }
 
             await Context.Interaction.RespondAsync(Translator.T().CmdEvidenceCreated(), ephemeral: true);
