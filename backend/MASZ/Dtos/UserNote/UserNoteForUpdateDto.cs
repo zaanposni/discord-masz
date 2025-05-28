6908ca628d3514e61ffad3fb0846da7783d4666c
controller <89075113+gg123bb@users.noreply.github.com>
2025-05-28 20:30:32 +0200

    Update UserNoteForUpdateDto.cs
    Bug fix, User id in Nodes is maxed to 18 chars but discord have longer id with 19 chars, id change it to 25, for the future we should delete the id-max.

--- backend/MASZ/Dtos/UserNote/UserNoteForUpdateDto.cs
+++ backend/MASZ/Dtos/UserNote/UserNoteForUpdateDto.cs
@@ -5,8 +5,10 @@ namespace MASZ.Dtos.UserNote
     public class UserNoteForUpdateDto
     {
         [Required(ErrorMessage = "UserId field is required", AllowEmptyStrings = false)]
-        [RegularExpression(@"^[0-9]{18}$", ErrorMessage = "the user id can only consist of numbers and must be 18 characters long")]
+        [RegularExpression(@"^[0-9]{25}$", ErrorMessage = "the user id can only consist of numbers and must be 18 characters long")]
+        /*The id-max is not good, because discord id's can getting longer, for long life suppor of the bot, we should delet the id-max value! i sett it now from 18 to 25*/
         public ulong UserId { get; set; }
+    
 
         [Required(ErrorMessage = "Description field is required", AllowEmptyStrings = false)]
         public string Description { get; set; }
